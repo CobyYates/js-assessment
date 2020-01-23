a18f9fa7c644bcfa4e949615730e0957afb7e483
cobylyates <cobylyates@gmail.com>
2020-01-22 20:36:35 -0700

    10 in Arrays complete

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,77 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove: function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == item) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr
   },
 
   removeWithoutCopy: function(arr, item) {
-
+    // let test = arr.map(item => item)
+    // test.length = 0
+    // return arr
   },
 
   append: function(arr, item) {
-
+    arr.push(item)
+    return arr
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item)
+    return arr
   },
 
   curtail: function(arr) {
-
+    arr.shift()
+    return arr
   },
 
   concat: function(arr1, arr2) {
-
+    let test = arr1.concat(arr2)
+    return test
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item)
+    return arr
   },
 
   count: function(arr, item) {
-
+    let count = arr.reduce((n, x) => n + (x === item), 0);
+    return count
   },
 
   duplicates: function(arr) {
-
+    console.log(arr)
+    let count = arr.reduce((n, x) => n = x, 0);
+    console.log(count)
+    return count
   },
 
-  square: function(arr) {
+  square: function(arr) {},
 
-  },
-
-  findAllOccurrences: function(arr, target) {
-
-  }
+  findAllOccurrences: function(arr, target) {}
 };
---
+++ whatilearned.md
@@ -0,0 +1,20 @@
+# What I learned
+
+### I learned that this is a great way to test my knowledge of js
+### This practice helped me to really get better at problem solving
+### Some examples of things I have never done, but have learned are:
+* Join two arrays together
+* Determin a location of an item in an array
+* Remove all instances of a value in an array
+* Count the occurances of an item in an array
+
+# How many tests was I able to pass
+### With the time I had with travelling and family, I was able to get 10 done in arrays. More to come though
+
+# Challenges/roadblocks
+### I struggled understanding how to remove all instances of a value from an array, returning the original array
+### Finding duplicates in an array
+### Some es6 syntax I found worked, but I don't quite understand the syntax yet. For example...
+```js
+let count = arr.reduce((n, x) => n + (x === item), 0)
+```
